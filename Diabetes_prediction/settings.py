ba146eeadf8fd8c6219b001fe168db972cf66ed7
Riya Horo <riyahoro74@gmail.com>
2025-03-22 15:54:41 +1100

    prepared for deployment

--- Diabetes_prediction/settings.py
+++ Diabetes_prediction/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ovmd1t4pkpwhz)v2b+6gb0rpfa!!9)*#zetlj@a%ip@5!m2ciw'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "fallback-secret-key-for-local-use")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
